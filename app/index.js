dfc6fb4a10b88aca38aa305439845949f002bbe6
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2016-09-05 18:11:07 +0000

    First commit with ract and redux

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import thunk from 'redux-thunk'
+// import { createStore, applyMiddleware } from 'redux'
+// import { Provider } from 'react-redux'
+
+import {Home} from 'components'
+
+// const store = createStore(users, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Home/>,
+document.getElementById('app'))
